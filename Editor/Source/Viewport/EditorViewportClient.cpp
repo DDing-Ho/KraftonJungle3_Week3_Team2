94ed510475247cd6b3c0fe1b28c62139df8c5b4c
Yonaim <yonaimyo@gmail.com>
2026-03-23 17:51:02 +0900

    Refactor: 렌더 확인용 데이터 변경

--- Editor/Source/Viewport/EditorViewportClient.cpp
+++ Editor/Source/Viewport/EditorViewportClient.cpp
@@ -1,6 +1,7 @@
 #include "Viewport/EditorViewportClient.h"
 #include "ApplicationCore/Input/InputRouter.h"
 #include "Engine/Scene.h"
+#include "Renderer/Types/EditorShowFlags.h"
 
 void FEditorViewportClient::Create()
 {
@@ -32,9 +33,9 @@ void FEditorViewportClient::Release()
 void FEditorViewportClient::Initialize(FScene* Scene, uint32 ViewportWidth, uint32 ViewportHeight)
 {
     CurScene = Scene;
-    
+
     ViewportCamera.OnResize(ViewportWidth, ViewportHeight);
-    
+
     SelectionController.SetActors(Scene->GetActors());
     SelectionController.SetCamera(&ViewportCamera);
     SelectionController.SetViewportSize(ViewportWidth, ViewportHeight);
@@ -70,14 +71,19 @@ void FEditorViewportClient::HandleInputEvent(const Engine::ApplicationCore::FInp
 
 void FEditorViewportClient::BuildRenderData(FEditorRenderData& OutRenderData) const
 {
-    OutRenderData.Gizmo.GizmoType = EGizmoType::Translation;
+    // OutRenderData.Gizmo.GizmoType = EGizmoType::Translation;
+     OutRenderData.Gizmo.GizmoType = EGizmoType::Rotation;
+    //OutRenderData.Gizmo.GizmoType = EGizmoType::Scaling;
     OutRenderData.Gizmo.Highlight = EGizmoHighlight::None;
-    OutRenderData.Gizmo.Transform = FMatrix::Identity;
+    OutRenderData.Gizmo.Frame = FMatrix::Identity;
+    OutRenderData.ShowFlags = EEditorShowFlags::SF_Grid | EEditorShowFlags::SF_WorldAxes |
+                              EEditorShowFlags::SF_Gizmo | EEditorShowFlags::SF_SelectionOutline |
+                              EEditorShowFlags::SF_ObjectLabels;
 }
 
 void FEditorViewportClient::OnResize(uint32 Width, uint32 Height)
 {
     ViewportCamera.OnResize(Width, Height);
-    
+
     SelectionController.SetViewportSize(Width, Height);
 }
